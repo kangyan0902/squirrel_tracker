c86b159f021056d702679c6b750d2db64dd0582e
kangyan0902 <kangyan0902@gmail.com>
2019-12-07 21:22:49 -0500

    add new files

--- squirrel_tracker/sightings/urls.py
+++ squirrel_tracker/sightings/urls.py
@@ -6,7 +6,5 @@ apps_name = 'sightings'
 urlpatterns = [
     path('', views.list_of_squirrels, name='list_of_squirrels'),
     path('add/', views.add_squirrel, name = 'add'),
-    path('stats/', views.stats, name='stats'),
     path('<str:squirrel_id>/', views.edit_squirrel, name='edit'),
-    path('<str:pk>/delete/', views.delete_squirrel.as_view(), name='delete'),
 ]
--- squirrel_tracker/squirrel_tracker/urls.py
+++ squirrel_tracker/squirrel_tracker/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('map/', include('map.urls'))
-    path('sightings/', include('sightings.urls'))
+    path('map/', include('map.urls')),
+    path('sightings/', include('sightings.urls')),
     path('admin/', admin.site.urls),
 
 ]
